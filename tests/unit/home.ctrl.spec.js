8ae273a08a711668439b39a6e615a1abc4ada621
Chiew Carol <carol.chiew@MY.ifastfinancial.local>
2016-05-10 08:36:13 +0800

    Add test spec for home ctrl

---
+++ tests/unit/home.ctrl.spec.js
@@ -0,0 +1,32 @@
+describe('MainCtrl', function() {
+    var scope,controller,stateParams;
+
+    // TODO: Load the App Module
+    beforeEach(function(){
+      module('app');
+    });
+
+    // TODO: Instantiate the Controller and Mocks
+    beforeEach(inject(function($controller,$rootScope) {
+      scope = $rootScope.$new();
+      stateParams = { name : 'user' }; //mock the stateParams value
+      controller = $controller("HomeCtrl as hc", { $scope: scope, $stateParams: stateParams });
+
+    }));
+
+    describe('- Initialization', function() {
+      it('should have a scope variable', function() {
+        expect(scope).toBeDefined();
+      });
+
+      it('should have controller initialized', function() {
+        expect(controller).toBeDefined();
+      })
+
+      it('should have name value in stateParams', function(){
+        expect(stateParams.name).toEqual('user');
+      });
+
+    });
+
+});

a8a4185ede2e179a0c69a67bd17b487b2abd57d3
Chiew Carol <kyaroru@outlook.com>
2016-05-11 14:32:41 +0800

    Test jenkin build mac

--- tests/unit/main.ctrl.spec.js
+++ tests/unit/main.ctrl.spec.js
@@ -1,12 +1,12 @@
 describe('MainCtrl', function() {
     var scope,controller,state;
 
-    // TODO: Load the App Module
+    // Load the App Module
     beforeEach(function(){
       module('app');
     });
 
-    // TODO: Instantiate the Controller and Mocks
+    // Instantiate the Controller and Mocks
     beforeEach(inject(function($controller,$rootScope,$state) {
       scope = $rootScope.$new();
       controller = $controller("MainCtrl as mc", { $scope: scope });
